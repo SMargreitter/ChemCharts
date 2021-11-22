6aa86818e741e5adec84c9ef37eb27c0389c9d14
Sophie Margreitter <sophie.margreitter@gmail.com>
2021-11-22 21:47:25 +0100

    binning

--- chemcharts/core/functions/binning.py
+++ chemcharts/core/functions/binning.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from rdkit import DataStructs
 import numpy as np
+import statistics
 
 from chemcharts.core.container.chemdata import ChemData
 
@@ -10,9 +11,54 @@ class Binning:
         pass
 
     @staticmethod
-    def binning(chemdata: ChemData) -> ChemData:
+    def _preparation(scores, num_bins):
+        bins = list(np.linspace(start=min(scores) - 0.1, stop=max(scores) + 0.1, num=num_bins))
+        bin_idx = list(np.digitize(scores, bins=bins) - 1)
+        sorted_bin_idx = list(set(bin_idx))
+        sorted_bin_idx.sort()
+        return sorted_bin_idx, bin_idx
+
+    @staticmethod
+    def _group_scores_bins(scores, sorted_bin_idx, bin_idx):
+        buffer = []
+        for item in sorted_bin_idx:
+            me = []
+            for idx in range(len(bin_idx)):
+                if item == bin_idx[idx]:
+                    me.append(scores[idx])
+            buffer.append(me)
+        return buffer
+
+    @staticmethod
+    def _calculate_medians(buffer):
+        median_list = []
+        for item in buffer:
+            if not item:
+                median_list.append(None)
+                continue
+            median_scores = statistics.median(item)
+            median_list.append(median_scores)
+        return median_list
+
+    @staticmethod
+    def _overwrite_scores_medians(bin_idx, median_list):
+        new_scores = []
+        for i_bin in range(len(bin_idx)):
+            # get current bin index for observation i_bin
+            cur_bin_idx = bin_idx[i_bin]
+
+            # append median for current bin index
+            new_scores.append(median_list[cur_bin_idx])
+        return new_scores
+
+    def binning(self, chemdata: ChemData, num_bins: int) -> ChemData:
         chemdata = deepcopy(chemdata)
-        np.linspace(start=-2.1, stop=8.0, num=3)
-        np.digitize([1.333, 2.33, -1, -2, -1.3, 7.9], bins=[-2.1, 2.95, 8.]) - 1
+        scores = list(chemdata.get_scores())
+        sorted_bin_idx, bin_idx = self._preparation(scores, num_bins)
+        buffer = self._group_scores_bins(scores, sorted_bin_idx, bin_idx)
+        median_list = self._calculate_medians(buffer)
+        new_scores = self._overwrite_scores_medians(bin_idx, median_list)
+
+        chemdata.set_scores(new_scores)
 
         return chemdata
--- tests/test_functions/test_binning/test_binning.py
+++ tests/test_functions/test_binning/test_binning.py
@@ -7,6 +7,8 @@ from chemcharts.core.container.chemdata import ChemData
 
 from chemcharts.core.container.smiles import Smiles
 from chemcharts.core.container.embedding import Embedding
+from chemcharts.core.container.fingerprint import *
+from chemcharts.core.functions.binning import Binning
 from chemcharts.core.functions.filtering import Filtering
 from chemcharts.core.functions.clustering import Clustering
 
@@ -20,73 +22,17 @@ class TestBinning(unittest.TestCase):
                          "C1ccc2c(c1)-c1ccc3ccccc3c1C2Cc1nn[nH]n1",
                          "Cc1ccccc1-c1c(C(=O)N=c2cccc[nH]2)cnc2ccccc12",
                          "N=c1[nH]c(=O)c2ncn(Cc3cccc4ccccc34)c2[nH]1",
-                         "O=C1c2cccc3c(F)ccc(c23)CN1c1cccnc1",
-                         "O=C(N=c1cccc[nH]1)c1cnc2ccccc2c1-c1ccc(Cl)cc1",
-                         "O=C(N=c1cccc[nH]1)c1cccc2cccc(-c3ccncn3)c12",
-                         "Cc1ccc(=NC(=O)c2cc(-c3c(F)cccc3F)nc3ccccc23)[nH]c1",
-                         "Cc1cc[nH]c(=NC(=O)c2cccc3c2-c2ccccc2C3=O)c1",
-                         "Cc1cccc(-c2ccc3c(=O)[nH]cc(-c4c[nH]c(=O)[nH]c4=O)c3c2)c1",
-                         "O=C1c2cccc3cccc(c23)CN1c1cccc(F)c1",
-                         "N=C1Cc(O)cc2c3c(c4ccccc4c2C1=O)C(=N)C(=O)C3=O",
-                         "Cc1cc[nH]c(=NC(=O)c2cccc3ccccc23)c1",
-                         "Cc1cc(Cl)cc(-c2c(C(=O)N=c3cccc[nH]3)ccc3ccccc23)c1",
-                         "C1=C(CN2CCNCC2)c2ccccc2-c2ccccc21",
-                         "O=C(N=c1cccc[nH]1)c1cccc2c(C(F)(F)F)nc3ccccc3c12",
-                         "Cc1cccc(-c2cc(C(=O)N=c3cc(C)cc(C)[nH]3)c3ccccc3n2)c1",
-                         "O=C(N=c1cncc[nH]1)c1cccc2c1-c1ccccc1C2=O"])
-        embedding_list = Embedding(np.array([[1, 2],
-                                             [2, 2],
-                                             [4, 2],
-                                             [2, 3],
-                                             [2.6, 5],
-                                             [11.2, 2],
-                                             [5, 1.2],
-                                             [4, 5],
-                                             [8, 9],
-                                             [1, 1],
-                                             [15, 1],
-                                             [5, 2],
-                                             [6, 4],
-                                             [6, 1],
-                                             [4, 18],
-                                             [3, 1],
-                                             [1, 9],
-                                             [6, 4],
-                                             [3, 4],
-                                             [7, 8]]))
-        fingerprint_list = FingerprintContainer("test_fingerprint",
-                                                [[1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-                                                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-                                                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-                                                 [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-                                                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                                                 [1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1],
-                                                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-                                                 [1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-                                                 [1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0],
-                                                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                                                 [1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-                                                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-        scores = [1, 3, 4, 5, 2, 1, 6, 3, 5, 0, 2, 1, 3, 4, 1, 2, 8, 6, 1, 1]
-        epochs = [0, 1, 1, 0, 2, 2, 0, 1, 0, 2, 3, 2, 3, 1, 0, 0, 3, 2, 1, 1]
+                         "O=C1c2cccc3c(F)ccc(c23)CN1c1cccnc1"])
+        scores = [1.333, 2.33, -1, 4.3, 7.9, 9.5, 5.1]
 
         test_data_set = ChemData(smiles)
-        test_data_set.set_embedding(embedding_list)
-        test_data_set.set_fingerprints(fingerprint_list)
         test_data_set.set_scores(scores)
-        test_data_set.set_epochs(epochs)
         cls.test_chemdata = test_data_set
 
     def test_binning(self):
-        pass
+        binning = Binning()
+        test_binning = binning.binning(self.test_chemdata, 4)
+        self.assertListEqual([1.333, 1.333, 1.333, 4.699999999999999, 8.7, 8.7, 4.699999999999999], test_binning.get_scores())
 
         #self.assertListEqual([6, 4], [int(x) for x in list(clustered_data.get_embedding()[0])])
         #self.assertEqual(score_length, 10)
