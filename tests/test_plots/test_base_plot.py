1e1117bc51a542d3bc77d1fcdf782423b3f9bf4d
percuisses <christian.margreitter@gmail.com>
2021-12-14 18:47:13 +0100

    Path changes.

--- tests/test_plots/test_base_plot.py
+++ tests/test_plots/test_base_plot.py
@@ -12,18 +12,16 @@ class TestBasePlot(unittest.TestCase):
         test_base_plot = BasePlot()
         test_updated_path = test_base_plot._path_update_snapshot(
             ori_path="../junk/base_plot_test/path_update.png", epoch_id=2)
-        self.assertEqual(
-            "/home/nutzer/Documents/Projects/ChemCharts/tests/junk/base_plot_test/0002_path_update.png",
+        self.assertIn(
+            "/base_plot_test/0002_path_update.png",
             test_updated_path)
 
         test_updated_path = test_base_plot._path_update_snapshot(
             ori_path="../junk/base_plot_test/path_update", epoch_id=6)
-        self.assertEqual(
-            "/home/nutzer/Documents/Projects/ChemCharts/tests/junk/base_plot_test/0006_path_update.png",
+        self.assertIn(
+            "/base_plot_test/0006_path_update.png",
             test_updated_path)
 
         test_updated_path = test_base_plot._path_update_snapshot(
             ori_path="../junk/base_plot_test/path_update.jpeg", epoch_id=12)
-        self.assertEqual(
-            "/home/nutzer/Documents/Projects/ChemCharts/tests/junk/base_plot_test/0012_path_update.png",
-            test_updated_path)
+        self.assertIn("junk/base_plot_test/0012_path_update.png", test_updated_path)
