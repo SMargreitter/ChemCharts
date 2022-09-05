19fea46345844493989bd1dd5cd9e67335503b4f
Esben Bjerrm <esben@odysseytx.com>
2022-09-05 09:36:19 +0000

    Added the multiplot column setting to other plot types and

--- src/chemcharts/core/plots/histogram_plot.py
+++ src/chemcharts/core/plots/histogram_plot.py
@@ -112,6 +112,7 @@ class HistogramPlot(BasePlot):
 
             self._merge_multiple_plots(subplot_paths=temp_plots_path_list,
                                        merged_path=final_path,
-                                       title=parameters.get(_PE.PARAMETERS_PLOT_TITLE, "Histogram ChemCharts Plot"))
+                                       title=parameters.get(_PE.PARAMETERS_PLOT_TITLE, "Histogram ChemCharts Plot"),
+                                       columns=parameters.get(_PE.PARAMETERS_MULTIPLOT_COLUMNS, None))
 
             self._clear_temp_dir(path=temp_folder_path)
